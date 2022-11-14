c0ab82436982ab6147e5ab001122c4c574d31e9d
Plazasesamo23 <114509099+Plazasesamo23@users.noreply.github.com>
2022-11-14 20:12:21 +0100

    Add files via upload
    nuevi

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-const typed = new Typed('.typed',{
+
+let typed;
+let escrito;
+typed = new Typed('.typed',{
     strings:['Proyecto','1'],
     typeSpeed:100,
     startDelay:300,
     backSpeed:100,
     loop:true
-    });
+    
+    
+});
 
+escrito = new Typed('.escrito',{
+    strings:['Pagina','de','inicio'],
+    typeSpeed:100,
+    startDelay:300,
+    loop:true,
+    backSpeed:100
+});
